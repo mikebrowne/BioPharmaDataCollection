781bc6013723a4316ad4dbaf75d3dce199e7a49b
Michael Browne <mikelcbrowne@gmail.com>
2019-06-18 17:35:32 -0600

    Some moving around of functions; Writing up docs

--- businesswire/scraper_functionality.py
+++ businesswire/scraper_functionality.py
@@ -13,28 +13,39 @@ Email: mikelcbrowne@gmail.com
 
 # IMPORTS
 import pandas as pd
-from bs4 import BeautifulSoup
-import time
-import numpy as np
 import re
+from utility_functions import get_page_as_soup
 
 
 # PUBLIC FUNCTIONS
 def scrape_search_pages(company_name, browser, num_pages=1):
+    '''
+    Searches and scrapes the resulting pages for article data
+    :param company_name: (str) - Company Name
+    :param browser: (Selenium WebBrowser)
+    :param num_pages: (int) - How many pages to go through
+    :return: (pd.DataFrame)
+    '''
     soups = get_search_pages(company_name, browser, num_pages=1)
     return pd.DataFrame(soups_to_data(soups)).T
 
 
-# PRIVATE HELPER FUNCTIONS
 def scrape_articles(article_links, browser):
+    '''
+    Goes through each article and collects the post
+    :param article_links: (list) - A list of the article URLs
+    :param browser: (Selenium WebBrowser)
+    :return: (list) - A list of the article writings as strings
+    '''
     return [scrape_individual_article(url, browser) for url in article_links]
 
 
+# PRIVATE HELPER FUNCTIONS
 def get_search_pages(company_name, browser, num_pages):
     soups = {}  # The key will be the page number, the value will be the data
     if num_pages == "all":
         # First get the number of pages
-        first_page_url = page_url(company_name, 1)
+        first_page_url = search_page_url(company_name, 1)
         first_page_soup = get_page_as_soup(first_page_url, browser)
         results = first_page_soup.find(class_="bw-search-results")
         num_pages = int(results.find_all("div")[-1].find_all("a")[-1].text)
@@ -42,7 +53,7 @@ def get_search_pages(company_name, browser, num_pages):
     # Iterate through the pages and get the soup objects
     for i in range(1, num_pages + 1):
         try:
-            url = page_url(company_name, i)
+            url = search_page_url(company_name, i)
             soups[i] = get_page_as_soup(url, browser)
         except Exception as e:
             # Note: Will build in a better system here for exception handling...
@@ -72,7 +83,7 @@ def list_item_to_data(li):
     }
 
 
-def page_url(company_name, page_number):
+def search_page_url(company_name, page_number):
     '''
     Get's the URL for the search page of a company
     :param company_name: (str)
@@ -83,23 +94,6 @@ def page_url(company_name, page_number):
     url_template_1 = r"https://www.businesswire.com/portal/site/home/search"
     url_template_2 = r"/?searchType=news&searchTerm={}&searchPage={}"
     return (url_template_1 + url_template_2).format(company_name, page_number)
-
-
-def get_page_as_soup(url, browser):
-    '''
-    Returns a BeautifulSoup object from a URL
-    :param url: (str) - URL link to a web page
-    :param browser: (obj) - Selenium Webdriver object
-    :return: (obj) - BeautifulSoup object
-    '''
-    browser.get(url)
-
-    time.sleep(np.random.randint(1, 6))
-
-    content = browser.page_source
-
-    soup = BeautifulSoup(content, "lxml")
-    return soup
 
 
 def scrape_individual_article(url, browser):
--- businesswire/utility_functions.py
+++ businesswire/utility_functions.py
@@ -5,7 +5,9 @@ utility_functions.py
 Various helper functions for use in the data scraper.
 
 '''
-
+from bs4 import BeautifulSoup
+import time
+import numpy as np
 
 def clean_name(name):
     '''Helper function to clean the name before use.'''
@@ -16,3 +18,19 @@ def clean_name(name):
     name = name.strip()
     return name
 
+
+def get_page_as_soup(url, browser):
+    '''
+    Returns a BeautifulSoup object from a URL
+    :param url: (str) - URL link to a web page
+    :param browser: (obj) - Selenium Webdriver object
+    :return: (obj) - BeautifulSoup object
+    '''
+    browser.get(url)
+
+    time.sleep(np.random.randint(1, 6))
+
+    content = browser.page_source
+
+    soup = BeautifulSoup(content, "lxml")
+    return soup
